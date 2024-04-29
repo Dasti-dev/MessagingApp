1c5df62170963d960879d6d8c31f759ebc5e8969
Dasti-dev <20cs3015@rgipt.ac.in>
2024-04-29 19:36:37 +0530

    Added Frontend functionality for OTP verification

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 function page() {
+
+  const [email,setEmail] = useState('');
+
   return (
     <div>
       sign in
--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -1,41 +1,85 @@
+'use client'
 import { useToast } from '@/components/ui/use-toast';
-import { signUpSchema } from '@/schemas/signUpSchema';
-import { verifySchema } from '@/schemas/verifySchema';
+// import { signUpSchema } from '@/schemas/signUpSchema';
 import { zodResolver } from '@hookform/resolvers/zod';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+import { ApiResponse } from '@/types/ApiResponse';
 import { useParams, useRouter } from 'next/navigation'
 import React from 'react'
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { verifySchema } from '@/schemas/verifySchema';
 
 function VerifyAccount() {
     const router = useRouter();
     const params = useParams<{username: string}>();
     const { toast } = useToast();
 
-    const form = useForm<z.infer<typeof signUpSchema>>({
-        resolver: zodResolver(signUpSchema),
-        defaultValues: {
-          username: '',
-          email: '',
-          password: ''
-        }
+    const form = useForm<z.infer<typeof verifySchema>>({
+        resolver: zodResolver(verifySchema),
     })
 
     const onSubmit = async (data: z.infer<typeof verifySchema>) => {
         try {
-            await axios.post('/api/verify-code',{
-                
+            const response = await axios.post('/api/verify-code',{
+                username: params.username,
+                code: data.code
             })
-        } catch(error) {
 
+            toast({
+                title: "Success",
+                description: response.data.message
+            })
+
+            router.replace('sign-in')
+        } catch(error) {
+            console.log('Error in verify endpoint of user', error);
+            const AxiosError = error as AxiosError<ApiResponse>;
+            let errorMessage = AxiosError.response?.data.message;
+            toast({
+              title: 'Verify failed',
+              description: errorMessage,
+              variant: 'destructive',
+            })
         }
     }
     return (
-        <div>
-        
+        <div className='flex justify-center items-center min-h-screen bg-gray-100'>
+            <div className='w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md'>
+                <div className='text-center'>
+                    <h1 className='text-4xl font-extrabold tracking-tight lg:text-5xl mb-6'>
+                        Verify Your Account
+                    </h1>
+                    <p className='mb-4'>
+                        Enter The Verification code sent to your email.
+                    </p>
+                </div>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+                        <FormField 
+                            control={form.control}
+                            name="code"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Verification Code</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder='code' {...field} />
+                                    </FormControl>
+                                    <FormMessage>
+
+                                    </FormMessage>
+                                </FormItem>
+                            )}
+                        />
+                        <Button type='submit'>Submit</Button>
+                    </form>
+                </Form>
+            </div>
         </div>
     )
 }
 
-export default VerifyAccount
+export default VerifyAccount;
